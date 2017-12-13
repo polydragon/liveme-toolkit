b8e6b56478e436efdb509cdf0f7f57216d5344f6
polydragon <30879984+polydragon@users.noreply.github.com>
2017-12-13 11:08:08 +0000

    change duration pipe to format-duration

--- src/app/pipes/duration.pipe.ts
+++ src/app/pipes/duration.pipe.ts
@@ -1,22 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as formatDuration from 'format-duration';
 
 @Pipe({
     name: 'duration'
 })
 export class DurationPipe implements PipeTransform {
     transform(value: any, args?: any): any {
-        let date = new Date(null);
-        date.setSeconds(value);
-        let final = date.toISOString().substr(11, 8);
-
-        if (final.startsWith('00:')) {
-            final = final.replace('00:', '');
-        }
-
-        if (final.startsWith('00:')) {
-            final = final.replace('00:', '');
-        }
-
-        return final;
+        return formatDuration(+value);
     }
 }
